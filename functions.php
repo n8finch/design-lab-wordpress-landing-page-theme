af124709ff47db8306e39e45973330c016f875da
Nate Finch <finchproservices@gmail.com>
2016-02-21 21:48:54 +0900

    added ACF to theme for XML uploads, etc.

--- functions.php
+++ functions.php
@@ -159,3 +159,6 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+
+include_once('advanced-custom-fields/acf.php');
